f95c9b0d3d7b0915276c6c7d4783ce0a4ba95610
Neel Patel <neelpatel1905.np@gmail.com>
2020-05-28 23:53:59 +0530

    feature: user profile
    - retrieval of user profile to show on user profile section

---
+++ student/get_profile.php
@@ -0,0 +1,22 @@
+<?php
+    require '../connection.php';
+    header('content-type: application/json');
+    $conn = getConn();
+    $id = $_POST['id'];
+    $data = array();
+
+    $query = "select first_name,last_name,email,mobile,date_of_birth,address,medium_id,class_id from login where id=$id";
+    $result = $conn->query($query);
+    $user = $result->fetch_array();
+
+    $result = $conn->query("select name from medium where id=".$user['medium_id']);
+    $medium = $result->fetch_array();
+
+    $result = $conn->query("select name from class where id=".$user['class_id']);
+    $class = $result->fetch_array();
+
+    $data = array("first_name"=>$user['first_name'],"last_name"=>$user['last_name'],"email"=>$user['email'],"mobile"=>$user['mobile'],"date_of_birth"=>$user['date_of_birth'],"address"=>$user['address'],"medium"=>$medium['name'],"class"=>$class['name']);
+
+    echo json_encode(array("data"=>$data));
+    
+?>
