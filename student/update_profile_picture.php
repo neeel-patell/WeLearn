ebb4bac0a2e8e7ff9228613483a6f06cbb6654e7
Neel Patel <neelpatel1905.np@gmail.com>
2020-05-29 17:20:22 +0530

    fix: profile response

--- student/update_profile_picture.php
+++ student/update_profile_picture.php
@@ -7,5 +7,7 @@
     $id = $_POST['id'];
     $image = $_POST['image'];
     file_put_contents("../images/profile/$id.jpg",base64_decode($image));
+    array_push($data,array("message"=>"Updated Successfully"));
+    echo json_encode(array("success"=>$data));
 
 ?>
