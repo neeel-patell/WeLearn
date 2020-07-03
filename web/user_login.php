bb2dcf6fec1927511d1d3c70d92282460069d037
Neel Patel <neelpatel1905.np@gmail.com>
2020-07-03 10:39:11 +0530

    fix: login, get_class api

--- web/user_login.php
+++ web/user_login.php
@@ -1,37 +1,41 @@
 <?php
     session_start();
-    $url = "localhost/welearn/login.php";
-    $fields = array(
-        "email"=>$_POST['email'],
-        "password"=>$_POST['pass']
-    );
-    $postvars = http_build_query($fields);
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POST, count($fields));
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $postvars);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/x-www-form-urlencoded"));
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    $result = curl_exec($ch);
-    curl_close($ch);
-    $result = json_decode($result);
-    if(isset($result->data)){
-        if($result->data->user_type == 0){
-            $_SESSION['login'] = $result->data->id;
-            $_SESSION['user_type'] = $result->data->user_type;
-            header("location: admin/");
-        }
-        else if($result->data->user_type == 1){
-            $_SESSION['login'] = $result->data->id;
-            $_SESSION['user_type'] = $result->data->user_type;
-            header("location: professor/");
+    require '../connection.php';
+    $conn = getConn();
+
+    $email = $_POST['email'];
+    $password = hash("sha256",$_POST['password']);
+
+    $query = "select user_type,active,id,first_name,last_name,class_id from login where email = '$email' and password = '$password'";
+    $result = $conn->query($query);
+    
+    if(mysqli_num_rows($result) != 0){
+        $row = $result->fetch_array();
+        if($row['active'] == 0){
+            header('location: index.php?msg=You are disabled by Admin');
         }
         else{
-            header("location: index.php?msg=Students can use only Application");
+            $user_type = $row['user_type'];
+            $_SESSION['login'] = $row['id'];
+            $_SESSION['user_type'] = $user_type;
+            if($user_type == 0){
+                header('location: admin/');
+            }
+            else if($user_type == 1){
+                header('location: professor/');
+            }
+            else{
+                header('location: index.php?msg=Students have to use Application');
+            }
         }
     }
     else{
-        header("location: index.php?msg=".print_r($result->error->message,true));
+        $result = $conn->query("select id from login where email = '$email'");
+        if(mysqli_num_rows($result) == 0){
+            header("location: index.php?msg=Email Not Registered");
+        }
+        else{
+            header('location: index.php?msg=Wrong Password');
+        }
     }
 ?>
